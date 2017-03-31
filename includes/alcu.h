9edbad0019d95fa17ea457582279b07685333662
Mathis Pandelé <mapandel@student.42.fr>
2017-03-31 20:42:13 +0200

    alcu.h 1.0 : Implementation

---
+++ includes/alcu.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   alcu.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/31 20:26:57 by mapandel          #+#    #+#             */
+/*   Updated: 2017/03/31 20:29:00 by mapandel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ALCU_H
+# define ALCU_H
+
+#include "../libft/libft.h"
+#include <fcntl.h>
+
+typedef struct			s_alcu
+{
+	//types
+}						t_alcu;
+
+//prototypes
+
+#endif
