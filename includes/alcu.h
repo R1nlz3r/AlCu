182eeab95b09fd8a2aae5be1e426e0553c43fccc
Mathis Pandelé <mapandel@student.42.fr>
2017-04-01 20:45:24 +0200

    alcu.h 1.2 : Nouvelles sources + t_alcu2

--- includes/alcu.h
+++ includes/alcu.h
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/31 20:26:57 by mapandel          #+#    #+#             */
-/*   Updated: 2017/04/01 15:54:54 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/04/01 20:00:13 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,20 @@ typedef struct			s_alcu
 	char				pad_0[4];
 }						t_alcu;
 
+typedef struct			s_alcu2
+{
+	int					scoreai;
+	int					scorehuman;
+}						t_alcu2;
+
 t_alcu					*init_add_t_alcu(t_alcu *alcu);
-void					del_t_alcu(t_alcu *alcu);
-void					display_error(t_alcu *alcu);
+t_alcu					*reset_position_t_alcu(t_alcu *alcu);
+void					del_t_alcu(t_alcu *alcu, t_alcu2 *alcu2);
+void					display_error(t_alcu *alcu, t_alcu2 *alcu2);
 void					display_start(void);
+void					display_board(t_alcu *alcu);
 t_alcu					*parsing_stdi(t_alcu *alcu, int *ret);
-//int						parsing_param(t_alcu *alcu, char **argv);
+t_alcu					*parsing_param(t_alcu *alcu, char **argv, int *ret);
+void					game(t_alcu *alcu, t_alcu2 *alcu2);
 
 #endif
