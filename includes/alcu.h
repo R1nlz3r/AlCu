d3ce82e22ca1e96e313161e8d141b592fdb9c29b
Mathis Pandelé <mapandel@student.42.fr>
2017-04-02 05:20:25 +0200

    alcu.h 1.3 : Nouvelles sources

--- includes/alcu.h
+++ includes/alcu.h
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/31 20:26:57 by mapandel          #+#    #+#             */
-/*   Updated: 2017/04/01 20:00:13 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/04/02 05:08:16 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,8 @@ typedef struct			s_alcu2
 {
 	int					scoreai;
 	int					scorehuman;
+	int					taken;
+	char				pad_1[4];
 }						t_alcu2;
 
 t_alcu					*init_add_t_alcu(t_alcu *alcu);
@@ -35,9 +37,15 @@ t_alcu					*reset_position_t_alcu(t_alcu *alcu);
 void					del_t_alcu(t_alcu *alcu, t_alcu2 *alcu2);
 void					display_error(t_alcu *alcu, t_alcu2 *alcu2);
 void					display_start(void);
-void					display_board(t_alcu *alcu);
+t_alcu					*display_board(t_alcu *alcu);
+void					display_score(t_alcu2 *alcu2);
+void					display_final_score(t_alcu2 *alcu2);
+void					display_human_plays(void);
+void					display_ai_plays(t_alcu2 *alcu2);
+void					display_human_won(t_alcu2 *alcu2);
+void					display_ai_won(t_alcu2 *alcu2);
 t_alcu					*parsing_stdi(t_alcu *alcu, int *ret);
 t_alcu					*parsing_param(t_alcu *alcu, char **argv, int *ret);
-void					game(t_alcu *alcu, t_alcu2 *alcu2);
+int		 				game(t_alcu *alcu, t_alcu2 *alcu2);
 
 #endif
