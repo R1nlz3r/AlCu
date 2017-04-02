fc1c2596031ad9195b8828ee3f4dafd986892906
Mathis Pandelé <mapandel@student.42.fr>
2017-04-02 20:21:04 +0200

    game.c 1.2 : Norme

--- sources/game.c
+++ sources/game.c
@@ -6,7 +6,7 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/01 18:48:03 by mapandel          #+#    #+#             */
-/*   Updated: 2017/04/02 05:08:02 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/04/02 20:20:08 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,67 @@ static int		ai_plays(t_alcu *alcu)
 
 }
 
+static int		human_plays(t_alcu *alcu, t_alcu2 *alcu2, char *buf)
+{
+	display_human_plays();
+	while (1)
+	{
+		read(1, buf, 10000);
+		if (buf[1] == '\n')
+		{
+			alcu2->taken = ft_atoi(buf);
+			if (alcu2->taken >= 1 && alcu2->taken <= 3
+				&& alcu->board - alcu2->taken >= 0)
+				break ;
+		}
+	}
+	return (alcu2->taken);
+}
+
+static int		plays(t_alcu *alcu, t_alcu2 *alcu2, char *buf, int *lock)
+{
+	alcu2->taken = human_plays(alcu, alcu2, buf);
+	alcu->board -= alcu2->taken;
+	if (!alcu->board)
+	{
+		ft_putstr("\033c");
+		display_ai_won(alcu2);
+		*lock = 2;
+		return (1);
+	}
+	alcu2->taken = ai_plays(alcu);
+	alcu->board -= alcu2->taken;
+	if (!alcu->board)
+	{
+		ft_putstr("\033c");
+		display_human_won(alcu2);
+		*lock = 2;
+		return (1);
+	}
+	*lock = 1;
+	return (0);
+}
+
+static void		game2(t_alcu *alcu, t_alcu2 *alcu2, char *buf, int *lock)
+{
+	while (alcu->board > 0)
+	{
+		if (*lock)
+		{
+			if (*lock == 1)
+			{
+				ft_putstr("\033c");
+				display_ai_plays(alcu2);
+			}
+		}
+		*lock = 0;
+		display_score(alcu2);
+		alcu = display_board(alcu);
+		if (plays(alcu, alcu2, buf, lock))
+			break ;
+	}
+}
+
 int				game(t_alcu *alcu, t_alcu2 *alcu2)
 {
 	char	*buf;
@@ -40,50 +101,7 @@ int				game(t_alcu *alcu, t_alcu2 *alcu2)
 		alcu = reset_position_t_alcu(alcu);
 		if (alcu && !alcu->board)
 			break ;
-		while (alcu->board > 0)
-		{
-			if (lock)
-			{
-				if (lock == 1)
-				{
-					ft_putstr("\033c");
-					display_ai_plays(alcu2);
-				}
-			}
-			lock = 0;
-			display_score(alcu2);
-			alcu = display_board(alcu);
-			display_human_plays();
-			while (1)
-			{
-				read(1, buf, 10000);
-				if (buf[1] == '\n')
-				{
-					alcu2->taken = ft_atoi(buf);
-					if (alcu2->taken >= 1 && alcu2->taken <= 3
-						&& alcu->board - alcu2->taken >= 0)
-						break ;
-				}
-			}
-			alcu->board -= alcu2->taken;
-			if (!alcu->board)
-			{
-				ft_putstr("\033c");
-				display_ai_won(alcu2);
-				lock = 2;
-				break ;
-			}
-			alcu2->taken = ai_plays(alcu);
-			alcu->board -= alcu2->taken;
-			if (!alcu->board)
-			{
-				ft_putstr("\033c");
-				display_human_won(alcu2);
-				lock = 2;
-				break ;
-			}
-			lock = 1;
-		}
+		game2(alcu, alcu2, buf, &lock);
 	}
 	display_final_score(alcu2);
 	ft_strdel(&buf);
