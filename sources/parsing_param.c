6c7c8852291166cf357d69dae7bad4c3686a405e
Mathis PANDELE <mapandel@e2r5p18.42.fr>
2017-04-02 22:14:03 +0200

    paring_param.c 1.1 : Norme

--- sources/parsing_param.c
+++ sources/parsing_param.c
@@ -6,40 +6,28 @@
 /*   By: mapandel <mapandel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/31 20:26:57 by mapandel          #+#    #+#             */
-/*   Updated: 2017/04/02 17:06:56 by mapandel         ###   ########.fr       */
+/*   Updated: 2017/04/02 21:48:13 by mapandel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "alcu.h"
 
-static t_alcu	*parsing_param2(t_alcu *alcu, int fd, int *ret)
+static t_alcu	*parsing_param3(t_alcu *alcu, char *buf, int *ret)
 {
-	char	*buf;
 	int		i;
 	int		j;
 	int		tmp;
 
-	if (!(buf = ft_strnew(10000))
-		|| (i = (int)read(fd, buf, 10000)) < 1 || i == 10000
-		|| (buf[i] = '\0') || buf[0] == '\n')
-	{
-		*ret = -1;
-		return (alcu);
-	}
 	i = 0;
 	while (buf[i])
 	{
-		if (buf[i] == '\n')
+		if (!(j = 0) && buf[i] == '\n')
 			++i;
 		if (!buf[i])
 			break ;
 		tmp = ft_atoi(&buf[i]);
-		j = 0;
-		while (ft_isdigit((int)buf[i]))
-		{
-			++i;
-			++j;
-		}
+		while (ft_isdigit((int)buf[i]) && ++i && ++j)
+			;
 		if (tmp < 1 || tmp > 10000 || j > 5 || buf[i] != '\n'
 			|| !(alcu = init_add_t_alcu(alcu)))
 		{
@@ -50,6 +38,22 @@ static t_alcu	*parsing_param2(t_alcu *alcu, int fd, int *ret)
 		alcu->board = tmp;
 	}
 	ft_strdel(&buf);
+	return (alcu);
+}
+
+static t_alcu	*parsing_param2(t_alcu *alcu, int fd, int *ret)
+{
+	int		i;
+	char	*buf;
+
+	if (!(buf = ft_strnew(10000))
+		|| (i = (int)read(fd, buf, 10000)) < 1 || i == 10000
+		|| (buf[i] = '\0') || buf[0] == '\n')
+	{
+		*ret = -1;
+		return (alcu);
+	}
+	alcu = parsing_param3(alcu, buf, ret);
 	return (alcu);
 }
 
